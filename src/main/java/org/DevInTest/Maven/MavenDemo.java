71acceeb2289fb2b90818217b5a64b0614034923
PriyaKathir18 <krishpriyaa@gmail.com>
2023-02-08 18:27:03 +0000

    Jenkins' collaboration try

---
+++ src/main/java/org/DevInTest/Maven/MavenDemo.java
@@ -0,0 +1,8 @@
+package org.DevInTest.Maven;
+
+public class MavenDemo {
+    public static void main(String[] args) {
+        System.out.println("Maven dependency management");
+    }
+
+}
---
+++ src/test/java/org/DevInTest/Maven/MavenTest.java
@@ -0,0 +1,18 @@
+package org.DevInTest.Maven;
+
+import org.junit.Test;
+
+public class MavenTest {
+
+    @Test
+    public void test()
+    {
+        System.out.println("Maven Test");
+    }
+
+    @Test
+    public void testGit()
+    {
+        System.out.println("Git Test");
+    }
+}
